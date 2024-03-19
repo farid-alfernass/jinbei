10b0ec7065c07f600dc822943fab3caa2f713c18
Farid Tri Wicaksono <farid.wicak@gmail.com>
2024-03-19 11:16:24 +0700

    fix: logger

--- helpers/utils/logger.js
+++ helpers/utils/logger.js
@@ -2,13 +2,14 @@ const winston = require('winston');
 const LogstashTransport = require('winston3-logstash-transport');
 const ecsFormat = require('@elastic/ecs-winston-format');
 const config = require('../../infra/configs/global_config');
+const morgan  = require('morgan');
 
 const logger = winston.createLogger({
   level: config.get('/appEnv') !== 'production' ? 'debug' : 'http',
   format: winston.format.combine( ecsFormat({ convertReqRes: true }), winston.format.timestamp(),  winston.format.json() ),
   transports: [
     new winston.transports.Console(),
-    // new LogstashTransport(config.get('/logstash'))
+    new LogstashTransport(config.get('/logstash'))
   ],
   exitOnError: false
 });
@@ -42,11 +43,57 @@ const log = (context, message, scope, data) => {
   logger.debug(logObject);
 };
 
+const init = () => {
+  return morgan((tokens, req, res) => {
+    const logData = {
+      method: tokens.method(req, res),
+      url: tokens.url(req, res),
+      code: tokens.status(req, res),
+      contentLength: tokens.res(req, res, 'content-length'),
+      responseTime: `${tokens['response-time'](req, res, '0')}`, // in milisecond (ms)
+      date: tokens.date(req, res, 'iso'),
+      ip: tokens['remote-addr'](req,res)
+    };
+    const obj = {
+      context: 'service-info',
+      scope: 'audit-log',
+      message: 'Logging service...',
+      meta: logData
+    };
+    logger.info(obj);
+    return;
+  });
+};
+
+const initLogger = () => {
+  return morgan((tokens, req, res) => {
+    const logData = {
+      method: tokens.method(req, res),
+      url: tokens.url(req, res),
+      code: tokens.status(req, res),
+      contentLength: tokens.res(req, res, 'content-length'),
+      responseTime: `${tokens['response-time'](req, res, '0')}`, // in milisecond (ms)
+      date: tokens.date(req, res, 'iso'),
+      ip: tokens['remote-addr'](req,res)
+    };
+    const obj = {
+      context: 'service-info',
+      scope: 'audit-log',
+      message: 'Logging service...',
+      meta: logData
+    };
+    logger.info(obj);
+    return;
+  });
+};
+
 module.exports = {
   enableLogging,
   error,
   warn,
   info,
   debug,
-  log
+  log,
+  initLogger,
+  init
 };
